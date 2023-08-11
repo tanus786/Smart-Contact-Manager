6532062df26cef1fe8040346069701c175dc9b3e
Tanu soni <Tanusoni2312@gmail.com>
2023-08-11 21:45:39 +0530

    hmm

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -16,15 +18,29 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@GetMapping("/index")
-	public String dashboard(Model model, Principal principal) {
-
+//Method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
 		String userName = principal.getName();
 //		System.out.println(userName);
 		// Get the use using userName(email)
 		User user = userRepository.getUserByUserName(userName);
 //		System.err.println(user);
 		model.addAttribute("user", user);
+	}
+
+//Home dashBoard
+	@GetMapping("/index")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("title", "Home || Dashboard");
 		return "normal/user_dashboard";
+	}
+
+//Add contact Controller
+	@GetMapping("/addContact")
+	public String addContact(Model m) {
+		m.addAttribute("title", "Add Contact");
+		m.addAttribute("contact", new Contact());
+		return "normal/addContact";
 	}
 }
