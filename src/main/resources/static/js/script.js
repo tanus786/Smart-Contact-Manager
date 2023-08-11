6cdab57ebe67f7485334a56f95e18ba313330055
Tanu soni <Tanusoni2312@gmail.com>
2023-08-11 19:01:51 +0530

    sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,19 +1,37 @@
-function toggleResetPswd(e){
-    e.preventDefault();
-    $('#logreg-forms .form-signin').toggle() // display:block or none
-    $('#logreg-forms .form-reset').toggle() // display:block or none
+
+//Dashboad Sidebar
+
+const toggleSidebar = () => {
+	if ($(".sidebar").is(":visible")) {
+		$(".sidebar").css("display", "none");
+		$(".content").css("margin-left", "0%");
+	}
+	else {
+		$(".sidebar").css("display", "block");
+		$(".content").css("margin-left", "20%");
+	}
+
+};
+
+
+
+
+function toggleResetPswd(e) {
+	e.preventDefault();
+	$('#logreg-forms .form-signin').toggle() // display:block or none
+	$('#logreg-forms .form-reset').toggle() // display:block or none
 }
 
-function toggleSignUp(e){
-    e.preventDefault();
-    $('#logreg-forms .form-signin').toggle(); // display:block or none
-    $('#logreg-forms .form-signup').toggle(); // display:block or none
+function toggleSignUp(e) {
+	e.preventDefault();
+	$('#logreg-forms .form-signin').toggle(); // display:block or none
+	$('#logreg-forms .form-signup').toggle(); // display:block or none
 }
 
-$(()=>{
-    // Login Register Form
-    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
-    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
-    $('#logreg-forms #btn-signup').click(toggleSignUp);
-    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+$(() => {
+	// Login Register Form
+	$('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+	$('#logreg-forms #cancel_reset').click(toggleResetPswd);
+	$('#logreg-forms #btn-signup').click(toggleSignUp);
+	$('#logreg-forms #cancel_signup').click(toggleSignUp);
 })
