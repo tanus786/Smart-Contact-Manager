635d822658548c51fb49e1b62f42bd3ca05bdadf
Tanu soni <Tanusoni2312@gmail.com>
2023-08-16 00:12:18 +0530

    Umm

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -2,8 +2,9 @@ package com.smart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SmartContactManagerApplication {
 
 	public static void main(String[] args) {
