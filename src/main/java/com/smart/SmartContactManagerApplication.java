bee1b56a84b240f943239a59f12eafe032fb935a
Tanu soni <Tanusoni2312@gmail.com>
2023-08-16 00:30:46 +0530

    ok

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -1,10 +1,12 @@
 package com.smart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableAutoConfiguration
 public class SmartContactManagerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/smart/dao/UserRepository.java
+++ src/main/java/com/smart/dao/UserRepository.java
@@ -2,11 +2,11 @@ package com.smart.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.smart.entities.User;
 
-@Component
+@Service
 public interface UserRepository extends JpaRepository<User,Integer>{
 	@Query("select u from User u where u.email = :email")
 	public User getUserByUserName(@Param("email") String email);
